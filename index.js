52614bda458169c1b7699e08e2466f8815881a47
Jon Roberts <kewlguy@gmail.com>
2020-09-09 14:58:01 -0600

    concept and styling done. now starting js

---
+++ index.js
@@ -0,0 +1,23 @@
+// Calcuator assignment
+
+// Render numbers
+// Render Functions
+// Render Display
+// Render Total
+
+
+
+// Take a number into first variable
+
+// Watch for "Math" function
+
+// Take a number into second variable
+
+// Do math based on function if clicked on equal
+
+    // Add
+    // Subtract
+    // Multiple
+    // Division (watch out for the zero set as second variable)
+
+//Return the value, display
