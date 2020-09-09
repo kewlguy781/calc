65ac6c948885dd7178269ad745598265fe99565e
Jon Roberts <kewlguy@gmail.com>
2020-09-09 15:15:19 -0600

    added some functions into the render pages

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // Calcuator assignment
 
 // Render numbers
+function renderNumber() {
+    return 'Working in progress';
+}
 // Render Functions
+function renderMath(){
+    return 'Working in progress';
+};
 // Render Display
+function renderDisplay(){
+    return 'Working in progress';
+};
 // Render Total
-
+function renderDisplay(){
+    return 'Working in progress';
+};
 
 
 // Take a number into first variable
@@ -21,3 +32,23 @@
     // Division (watch out for the zero set as second variable)
 
 //Return the value, display
+
+//Page Render
+function render() {
+    let htmlString = `
+    <center><h1> Javascript calculator </h1>
+    <div class="display">
+    ${renderDisplay()}
+    </div>
+    <div class="calc_home">
+    ${renderNumber()}
+    </div>
+    <div class="math">
+    ${renderMath()}
+    </div>
+    </center>
+    `;
+    document.getElementById("apps").innerHTML = htmlString;
+}
+
+render();
